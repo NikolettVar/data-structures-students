458c43e168c99064e026f2331bb8e9d3ade16337
NikolettVar <49991561+NikolettVar@users.noreply.github.com>
2021-07-03 23:54:45 +0100

    Update StudentApp.java

--- StudentApp.java
+++ StudentApp.java
@@ -11,13 +11,8 @@ package dssampletest;
  * 15 03 2021
  */
 public class StudentApp {
-
-    /**
-     * @param args the command line arguments
-     */
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         
         LinkedList students = new LinkedList();
 
